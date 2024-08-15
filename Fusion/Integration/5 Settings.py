cdbea9dfa15efd9786cd3ddf1d49a1f9beb40604
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-08-15 07:21:02 -0400

    Delete Fusion/Integration/5 Settings.py
    File was renamed.

--- Fusion/Integration/5 Settings.py
+++
@@ -1,7 +0,0 @@
-from CallButtons import CallButton
-# from ResetPrism import launch_prismFusion_menu
-# from PrismInit import checkThirdParty
-import openPrismWindows
-if __name__ == "__main__":
-    # checkThirdParty()
-    CallButton('btn_projectbrowser')
