f33d8bf0e77a6d4edb6f3ffeb13d72acd2e61379
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-08-15 07:18:48 -0400

    Revised menu and file naming scheme
    Removed the numbering for the menu items and revised the naming scheme of some files.

---
+++ Fusion/Integration/Menu_OpenStateManager.py
@@ -0,0 +1,12 @@
+from CallButtons import CallButton
+# from ResetPrism import launch_prismFusion_menu
+# from PrismInit import checkThirdParty
+# import openPrismWindows
+
+
+if __name__ == "__main__":
+    # checkThirdParty()
+    # try:
+    CallButton('btn_statemanager')
+    # except:
+    #     openPrismWindows.openPrismStateManager()
---
+++ Fusion/Integration/Menu_SaveVersion.py
@@ -0,0 +1,8 @@
+# from openPrismWindows import runPrismSaveScene
+# from PrismInit import checkThirdParty
+from CallButtons import CallButton
+
+
+if __name__ == "__main__":
+    # checkThirdParty()
+    CallButton("btn_saveversion")
