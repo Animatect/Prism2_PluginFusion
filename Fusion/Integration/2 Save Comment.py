643b2d5b9791a386079ea29956d097bbb9e70432
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-08-15 07:20:29 -0400

    Delete Fusion/Integration/2 Save Comment.py
    File was renamed.

--- Fusion/Integration/2 Save Comment.py
+++
@@ -1,6 +0,0 @@
-# from openPrismWindows import openPrismSaveWithComment
-# from PrismInit import checkThirdParty
-from CallButtons import CallButton
-if __name__ == "__main__":
-    # checkThirdParty()
-    CallButton("btn_savecomment")
