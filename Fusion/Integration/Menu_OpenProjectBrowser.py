65bd30eaf6d5fe8374764187b0000d96ca4ed212
Joshua Breckeen <86539171+AltaArts@users.noreply.github.com>
2024-08-15 07:18:48 -0400

    Revised menu and file naming scheme
    Removed the numbering for the menu items and revised the naming scheme of some files.

---
+++ Fusion/Integration/Menu_OpenProjectBrowser.py
@@ -0,0 +1,7 @@
+from CallButtons import CallButton
+# from PrismInit import checkThirdParty
+
+
+if __name__ == "__main__":
+    # checkThirdParty()
+    CallButton('btn_projectbrowser')
---
+++ Fusion/Integration/Menu_SaveComment.py
@@ -0,0 +1,8 @@
+# from openPrismWindows import openPrismSaveWithComment
+# from PrismInit import checkThirdParty
+from CallButtons import CallButton
+
+
+if __name__ == "__main__":
+    # checkThirdParty()
+    CallButton("btn_savecomment")
